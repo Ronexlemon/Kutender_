f3448a8d38cf143ebfb79eeb5232ad180e12b219
RonexLemon <ronexondimu@gmail.com>
2023-09-20 18:00:30 +0300

    bug

--- client/src/pages/bidders/DisplayMyBids.jsx
+++ client/src/pages/bidders/DisplayMyBids.jsx
@@ -38,8 +38,8 @@ const DisplayBidsTenders = (props) => {
                     key={tender.companyName + index}
                     className="border-b border-gray-200  hover:bg-gray-100"
                   >
-                    {tender.bidowner.toString().toLowerCase() ===
-                      props.userAccount.toString().toLowerCase() && (
+                    {tender.bidowner ==
+                      props.userAccount && (
                       <>
                         <td className="py-3 px-2 text-left whitespace-nowrap">
                           <div className="flex items-center">
